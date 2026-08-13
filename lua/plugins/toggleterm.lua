2077293ad4685df47b2576f9d703f26af11be892
Thomas Kirschner <thomaskirschner85@yahoo.de>
2026-08-13 19:24:46 +0200

    Remove opencode and add shortcuts for toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,24 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      direction = "horizontal",
+      size = 15,
+    },
+    keys = {
+      {
+        "<C-.>",
+        "<Cmd>ToggleTerm direction=horizontal<CR>",
+        mode = { "n", "t" },
+        desc = "Toggle Terminal",
+      },
+      {
+        "<C-.>",
+        "<Esc><Cmd>ToggleTerm direction=horizontal<CR>",
+        mode = "i",
+        desc = "Toggle Terminal",
+      },
+    },
+  },
 }
