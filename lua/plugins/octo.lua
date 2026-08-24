52ca2bd9ee4f4beedc95378ddafc4d2d89cdef01
Thomas Kirschner <thomaskirschner85@yahoo.de>
2026-08-24 19:34:52 +0200

    Removed project v2

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,6 +1,10 @@
 return {
   {
     "pwntester/octo.nvim",
+    opts = {
+      -- PR review does not need GitHub Projects v2 or its read:project token scope.
+      default_to_projects_v2 = false,
+    },
     keys = {
       -- Keep Telescope's <leader>gS for Git stash.
       { "<leader>gS", false },
